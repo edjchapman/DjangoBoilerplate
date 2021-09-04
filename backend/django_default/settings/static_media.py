7f68b8b22d5f4b089eca293ac49cc1f8f3b0282b
ed <edchapman88@gmail.com>
2021-09-04 02:15:42 +0100

    Successful media upload in development.

--- backend/django_default/settings/static_media.py
+++ backend/django_default/settings/static_media.py
@@ -1,9 +1,7 @@
-import os
-
 from .vars import BASE_DIR
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.getenv('DJANGO_MEDIA_ROOT')
+MEDIA_ROOT = BASE_DIR / "mediafiles"
